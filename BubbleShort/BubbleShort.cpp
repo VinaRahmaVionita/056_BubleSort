ea3e51c705febda6b852dc2feaf4df8afbdbbc71
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-17 16:34:19 +0700

    menambah prosedur display

--- BubbleShort/BubbleShort.cpp
+++ BubbleShort/BubbleShort.cpp
@@ -46,3 +46,14 @@ void unsorted() {
 	}
 	cout << endl;
 }
+
+void display() {													//prosedur untuk menampilkan hasil 
+	cout << endl;													//output baris kosong 
+	cout << "==================================" << endl;			//output ke layar 
+	cout << "Elemen Array yang telah tersusun" << endl;				//output ke layar 
+	cout << "==================================" << endl;			//output ke layar 
+	for (int j = 0; j < n; j++) {									//looping dengan j di mulai dari 0 hingga n-1
+		cout << a[j] << endl;										//output ke layar 
+	}
+	cout << endl;													//output baris kosong 
+}
