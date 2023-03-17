b82ae03c87938a1b031b0b4c24cd327871e93e47
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-03-17 16:27:52 +0700

    menambah prosedur unsored

--- BubbleShort/BubbleShort.cpp
+++ BubbleShort/BubbleShort.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int a[20];				//Deklarasi array a dengan ukuran 20
 int n;					//Deklarasi variable n untuk menyimpan banyaknya elemen pada array 
 
-void input() {			//Procedur untuk input 
-	while (true) {		//Looping
+void input() {													//Procedur untuk input 
+	while (true) {												//Looping
 		cout << "Masukkan banyaknya elemen pada array : ";		//Output ke layar
-		cin >> n;		//Input dari pengguna
+		cin >> n;												//Input dari pengguna
 		if (n <= 20)	//Jika n kurang dari atau sama dengan 20
 			break;		//Keluar dari loop
 		else {			//Jika n lebih dari 20 
@@ -19,12 +19,12 @@ void input() {			//Procedur untuk input
 	cout << "Masukkan Elemen Array" << endl;	//Output ke layar
 	cout << "===================" << endl;		//Output ke layar
 
-	for (int i = 0; 1 < n; i++) {		//Looping dengan i di mulai dari 0 hingga n-i 
+	for (int i = 0; 1 < n; i++) {					//Looping dengan i di mulai dari 0 hingga n-i 
 		cout << "Data ke-" << (i + 1) << ": ";		//Output ke layar
 		cin >> a[i];								//Input dari pengguna 
 	}
-}
-void bubbleSortArray() {			//prosedur untuk mengurutkan array dengan metode bubble sort
+}	
+void bubbleSortArray() {						//prosedur untuk mengurutkan array dengan metode bubble sort
 	for (int i = 1; i < n; i++) {				//looping dengan i di mulai dari 1 hingga n-1
 		for (int j = 0; j < n - i; j++) {		//looping dengan j di mulai dari 0 hingga n-1
 			if (a[j] > a[j + 1]) {				//jika nilai pada a[j] lebih besar dari a[j+1]
@@ -34,4 +34,15 @@ void bubbleSortArray() {			//prosedur untuk mengurutkan array dengan metode bubb
 			}
 		}
 	}
+}
+
+void unsorted() {
+	cout << endl;												//output baris kosong 
+	cout << "==================================" << endl;		//output ke layar
+	cout << "Elemen Array yang belum tersusun" << endl;			//output ke layar 
+	cout << "==================================" << endl;		//output ke layar
+	for (int j = 0; j < n; j++) {								//looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j], , endl;									//output ke layar
+	}
+	cout << endl;
 }
